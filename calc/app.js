ef4c89b83c407a288291276ba33155be23d18a2f
Rodrigue-web-coder <69993181+Rodrigue99@users.noreply.github.com>
2020-09-18 17:25:23 +0100

    Add files via upload

---
+++ calc/app.js
@@ -0,0 +1,14 @@
+function presser(touche) {
+    document.getElementById('resultat').innerHTML+=touche;
+}
+
+function calculer() {
+    var expression = document.getElementById('resultat').innerHTML;
+    var resultat_a_afficher = math.evaluate(expression);
+    document.getElementById('resultat').innerHTML = resultat_a_afficher;
+}
+
+function effacer() 
+ { 
+     document.getElementById("resultat").innerHTML = "" ;
+ } 
